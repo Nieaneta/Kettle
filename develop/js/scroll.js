b9bced2e85ee2612a8c11f86baff89a8bd5b949e
Aneta <nieaneta@int.pl>
2019-06-05 20:41:00 +0200

    add smooth scroll

--- develop/js/scroll.js
+++ develop/js/scroll.js
@@ -3,7 +3,7 @@ sr.reveal(
     ".history-title, .history-know, .history-text, .why-title, .why-txt,.why-text, .forWho-title, .forWho-txt, .forWho-text, .amrap-title, .amrap-txt, .amrap-text, .arr-title",
     {
         reset: true,
-        delay: 400
+        delay: 300
     }
 );
 
@@ -14,20 +14,3 @@ style = "visibility: visible";
 ("opacity: 1");
 ("transition: transform 0.5s cubic-bezier(0.6, 0.2, 0.1, 1) 0.3s, opacity 0.5s cubic-bezier(0.6, 0.2, 0.1, 1) 0.3s; ");
 
-// window.sr = ScrollReveal();
-// sr.reveal(
-//     ".history-title, .history-know, .history-text, .why-title, .why-txt,.why-text",
-//     {
-//         reset: true,
-//         delay: 300
-//     }
-// );
-
-// style = "visibility: visible";
-// ("transform: translateY(20px) scale(0.9)");
-// ("opacity: 0");
-// ("transform: translateY(20px) scale(0.9)");
-// ("opacity: 0");
-// ("transition: transform 0.5s cubic-bezier(0.6, 0.2, 0.1, 1) 0s",
-// ("opacity 0.5s cubic-bezier(0.6, 0.2, 0.1, 1) 0s)",
-// ("transition: transform 0.5s cubic-bezier(0.6, 0.2, 0.1, 1) 0s, opacity 0.5s cubic-bezier(0.6, 0.2, 0.1, 1) 0s;");
